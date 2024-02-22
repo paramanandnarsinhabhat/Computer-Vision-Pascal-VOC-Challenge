6341cee5b05de564add7d536ffbb16686acdcf60
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-22 15:00:53 +0530

    Update pascal_voc.py

--- source/pascal_voc.py
+++ source/pascal_voc.py
@@ -7,3 +7,16 @@ from PIL import Image
 train_annotations = pd.read_csv('/Users/paramanandbhat/Downloads/dataset_pascalVOCDetection-200625-193221/train.csv')
 
 print(train_annotations.head())
+
+# Function to visualize an image with its bounding boxes
+def visualize_image(image_path, annotations):
+    img = Image.open(image_path)
+    fig, ax = plt.subplots(1)
+    ax.imshow(img)
+
+    for index, row in annotations.iterrows():
+        box = [row['xmin'], row['ymin'], row['xmax'] - row['xmin'], row['ymax'] - row['ymin']]
+        rect = patches.Rectangle((box[0], box[1]), box[2], box[3], linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    plt.show()
+
