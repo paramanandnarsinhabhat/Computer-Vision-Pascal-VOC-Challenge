9d5297218018a976151876930dbd85754c561936
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-22 13:08:31 +0530

    install required packages

--- source/pascal_voc.py
+++ source/pascal_voc.py
@@ -4,6 +4,8 @@ import torchvision.transforms as transforms
 # Load a pretrained model, e.g., Faster R-CNN
 model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
+model.eval()  # Set the model to inference mode
+
 
 
 
